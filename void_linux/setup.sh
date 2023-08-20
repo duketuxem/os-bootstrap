8de7a9ca317d09d7ccc8948c343f872fdae145db
Duke Tuxem <33418846+DukeTuxem@users.noreply.github.com>
2023-08-21 00:42:36 +0200

    Trying to set remote to ssh

--- void_linux/setup.sh
+++ void_linux/setup.sh
@@ -10,31 +10,32 @@ distro_directory="$(pwd)"
 info "Creating the home folder structure..."
 create_home_folder_structure
 
-
 info "Installing the user packages..."
 call sudo xbps-install -y $(grep -v '^#' ./packages.txt | tr '\n' ' ') \
-&& success "Package requirements satisfied!\n"
+	&& success "Package requirements satisfied!\n"
 
+cd "$ricing_project_directory"
 
 info "Installing the suckless tool suite..."
-cd "$ricing_project_directory"
 for soft in dmenu dwm st; do
 	call git clone https://github.com/duketuxem/$soft.git -b my_fork \
-	&& cd $soft && call sudo make install 2>&1 > /dev/null && cd .. \
-	&& success "$soft successfully installed!\n"
-done
+		&& cd $soft && call sudo make install > /dev/null 2>&1 \
+		&& success "$soft successfully installed!\n"
 
+	call git remote set-url origin git@github.com:duketuxem/$soft.git \
+		&& success "Repository set to use SSH."
+done
 
 info "Setting up all the dotfiles..."
 call git clone https://github.com/duketuxem/dotfiles.git -b setup
 call cp -r "./dotfiles/.config" "$HOME"
 call cp -r "./dotfiles/.local" "$HOME"
-call git clone --bare https://github.com/duketuxem/dotfiles .dotfiles
-
+call git clone --bare https://github.com/duketuxem/dotfiles "$HOME"/.dotfiles
+call git --git-dir="$HOME"/.dotfiles \
+	remote set-url origin git@github.com:duketuxem/$soft.git
 
 info "Override the default Zsh configuration location to match ours"
-call sudo sh -c 'printf "export ZDOTDIR=\"\$HOME\"/.config/zsh\n" > /etc/zsh/zshenv'
-
+sudo sh -c 'printf "export ZDOTDIR=\"\$HOME\"/.config/zsh\n" > /etc/zsh/zshenv'
 
 info "Changing to Zsh"
 call chsh -s /bin/zsh
