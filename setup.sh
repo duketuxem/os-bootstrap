5468c5cb91e95459321e3afb7a3b3a397c75161a
Duke Tuxem <33418846+DukeTuxem@users.noreply.github.com>
2024-01-14 13:55:47 +0100

    add checks - to test

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env sh
 
+# This script aims to be POSIX compatible (no shell flavor).
+# As a reminder there are no arrays, all variables are global, ...
+
+# Load the configuration (variables) to be used here
 . ./config.sh
+
+error=0
+
+# Prevent git from asking to supply information in an interactive way.
+# This makes all the checks regarding remote repositories possible.
+GIT_ASKPASS="echo"
+SSH_ASKPASS="echo"
 
 ###
 # Utils
@@ -8,6 +19,7 @@
 error()
 {
     printf "\033[0;1;31m$*\033[0m\n"
+    error=1
 }
 
 success()
@@ -65,6 +77,10 @@ ask()
 	fi
 }
 
+
+###
+# Actual script
+###
 which_linux()
 {
 	if has lsb_release; then
@@ -102,102 +118,58 @@ which_linux()
 	[ $? -eq 1 ] && exit 1
 }
 
-check_requirements()
+create_home_folder_structure()
 {
-	# Is the folder for hosting the repositories existing ?
-	mkdir -p "$ricing_project_directory" 2> /dev/null
-	[ $? -ne 0 ] \
-		&& error "The project folder does not exist and can not be created" \
-		&& exit 1
-
-	# TODO: Does the working directory == root of the repository ?
-	# ls == expected files
-
-	# TODO: Are the package names only alphanum chars ?
-}
-
-create_home_folder_structure() {
-	# NOW DEPRECATED: This is to change
-	# TODO: use a rsync kind of mechanism instead to establish the folder hierarchy
-	# Those folders are expected by my dotfiles before they are deployed.  I
-	# try to maintain a clean home as much as possible by using the
-	# environment variables relative to the XDG Base Directory
-	# specification.
-	#
-	# The 'bin/' and 'log/' directories are conceptually extending the
-	# previous concept a bit more, storing scripts and desktop related
-	# software logs to one specific place.
-	mkdir "$HOME/.config"			\
-			"$HOME/.local"		\
-			"$HOME/.local/cache"	\
-			"$HOME/.local/share"	\
-			"$HOME/.local/state"	\
-						\
-			"$HOME/.local/bin"	\
-			"$HOME/.local/log"	2> /dev/null
+	find "$home_template/$main_profile_directory" -type d \
+		| xargs -I % mkdir %
 }
 
 # optimization: detect if any package is missing before running the
 # package manager command
 install_packages()
 {
-	# Install the core packages.
-	if [ ! -f ./"$platform_folder"/core.txt ]; then
-		error "Can not find the $platform_folder/core.txt file."
-		exit 1
-	fi
-	core_packages=$(grep -v '^#' "./$platform_folder/core.txt" | tr -s '\n' ' ')
-	install_command="$privilege_escalation $package_manager $core_packages"
+	packages=$(grep -v '^#' "./$platform_folder/$main_profile_file" | tr -s '\n' ' ')
+	install_command="$privilege_escalation $package_manager $packages"
 	printf "Running: $install_command\n"
-	ask "Proceed ?"
-	[ $? -eq 1 ] && return
-	eval "$install_command"
 
-	# Install the desktop profile if the user wants it.
-	ask "Do you want to install the desktop profile to have a GUI ?"
-	[ $? -eq 1 ] && return
-	if [ ! -f ./"$platform_folder"/gui.txt ]; then
-		error "Can not find the $platform_folder/gui.txt file."
+	if ask "Proceed ?"; then
 		exit 1
 	fi
-	gui_packages=$(grep -v '^#' "./$platform_folder/gui.txt" | tr -s '\n' ' ')
-	install_command="$privilege_escalation $package_manager $gui_packages"
-	printf "Running: $install_command\n"
-	ask "Proceed ?"
-	[ $? -eq 1 ] && return
-	eval "$install_command"
-	[ $? -ne 0 ] \
-		&& error "Something failed during the packages installation." \
-		&& exit 1
+	if eval "$install_command"; then
+		error "Something failed during the packages installation."
+		exit 1
+	fi
+
+	# =======================================================================
 
 	# TODO Make this not hardcoded...
-	step "Installing the custom desktop suite (suckless)..."
-	cd "$ricing_project_directory"
-	for soft in dmenu dwm st
-   	do
-		if has $soft; then
-			info "$soft is already installed"
-			continue
-		fi
-		call git clone https://github.com/duketuxem/"$soft".git -b my_fork \
-			&& cd $soft \
-			&& call "$privilege_escalation" make install \
-			&& success "$soft successfully installed!\n" \
-			&& git remote set-url origin git@github.com:duketuxem/$soft.git \
-			&& success "Repository set to use SSH." \
-			&& cd ..
-	done
+	# step "Installing the custom desktop suite (suckless)..."
+	# cd "$repositories_location"
+	# for soft in dmenu dwm st
+   	# do
+	# 	if has $soft; then
+	# 		info "$soft is already installed"
+	# 		continue
+	# 	fi
+	# 	call git clone https://github.com/duketuxem/"$soft".git -b my_fork \
+	# 		&& cd $soft \
+	# 		&& call "$privilege_escalation" make install \
+	# 		&& success "$soft successfully installed!\n" \
+	# 		&& git remote set-url origin git@github.com:duketuxem/$soft.git \
+	# 		&& success "Repository set to use SSH." \
+	# 		&& cd ..
+	# done
 }
 
 install_dotfiles()
 {
 	# In case the GUI profile is not installed, we're still in the repo.
-	cd "$ricing_project_directory"
+	cd "$repositories_location"
 	# Clone the repository sources using the right branch.
-	call git clone "$dotfiles_repo_url_http" -b "$dotfiles_repo_branch"
+	call git clone "$dotfiles_repo_url_http" -b "$dotfiles_git_branch"
 	# 'deploy' the configuration TODO: Use `rsync` here ?
-	call cp -r "./$dotfiles_repo_name/.config"	\
-		"./$dotfiles_repo_name/.local"			\
+	call cp -r "./$dotfiles_git_repo/.config"	\
+		"./$dotfiles_git_repo/.local"			\
 		"$HOME"
 	# Setup the dotfiles workflow using git bare repositories
 	call git clone --bare "$dotfiles_repo_url_http" "$HOME/.dotfiles"
@@ -218,32 +190,86 @@ change_default_shell() {
 	call chsh -s "$login_shell"
 }
 
+# Attempt to detect any inconsistence that would raise an error later
+# Also helps to make the script replayable
+check_requirements()
+{
+	step "Checking requirements"
+	#
+	if mkdir -p "$repositories_location" 2> /dev/null;
+	then
+		error "The folder holding repos doesn't exist and can't be created"
+	fi
 
-# =============================================================================
+	# Required for creating the home folder structure
+	# devnote: let's not handle some cases where the script is run
+	# from outside the working directory. Make this a requirement, over.
+	if [ "$(pwd | awk -F '/' '{print $NF}')" != "os-bootstrap" ];
+	then
+		error "Please, cd to the os-bootstrap cloned repository"
+	fi
 
-step "Detecting platform"
+	# Check if all the packages can be resolved
+	for file in "$platform_folder"/*;
+	do
+		# TODO: test | alternative otherwise : "${string#*"$word"}"
+		case "$main_profile_file $desktop_profile_file" in
+		  *$main_profile_file*) 	;;
+		  *$desktop_profile_file*) 	;;
+		  *)	error "Unexpected profile text file" ;;
+		esac
+
+		# Concatenate all lines representing packages from the current
+		# text file and verify whether the package manager accepts
+		# all of them.
+		packages=$(awk '/^[a-zA-Z0-9]/ {printf "%s ", $0} END{print ""}' "$file")
+		# TODO: --dry-run is not implemented in every package managers
+		# so far as I checked : xbps / apt / not rpm...
+		"$package_manager" --dry-run "$packages" > /dev/null 2>&1
+		if [ $? -ne 0 ]; then
+		       error "Some package seems incorrect from :"
+		       "$package_manager" --dry-run "$packages" > dev/null
+		fi
+       	done
+
+	### Check if the dotfiles repository exists
+	# --exit-code allows the command to fail if the branch is incorrect
+	if git ls-remote --exit-code \
+		"$dotfiles_git_full_url" \
+	       	"$dotfiles_git_branch" > /dev/null 2>&1;
+	then
+		error "The 'dotfiles' repo or branch is incorrect"
+	fi
+
+	[ $error -ne 0 ] && exit $error
+
+	success "All requirements satisfied"
+	sleep 1
+}
+
+
+
 operating_system="$(uname -s)"
 
 if [ "$operating_system" = "Linux" ]
 then
-	# The 'related' notes below are here to help to distinguish the functionnal
-	# concepts of this scripts.
+	# Distro related
 	platform_folder='NONE'
 	package_manager='NONE'
-	which_linux							# related : os dectection
+	which_linux
 
-	step "Checking requirements"
 	check_requirements
-
-	# related : directory spec
-	step "Creating the expected home folder structure if not present"
-	create_home_folder_structure
 
 	step "Installing the packages"
 	# related : core / gui profiles
 	# direct adherences to dotfiles (wm)
-	# for GUI: which graphic server
+	# for Desktop: which graphic server
 	install_packages
+	exit 0
+
+	# related : directory spec
+	step "Creating the expected home folder structure if not present"
+	create_home_folder_structure
 
 	step "Importing the dotfiles configuration"
 	# related: other reposiory
@@ -260,6 +286,6 @@ then
 	success "The script has finished the install. Enjoy."
 else
 	error "Unsupported platform"
-	exit 1
+	return 1
 fi
-exit 0
+return 0
