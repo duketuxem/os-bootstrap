7886f46831d2c3e5c806550fa326880701bfd972
Duke Tuxem <33418846+DukeTuxem@users.noreply.github.com>
2024-01-19 22:48:19 +0100

    Separate profile description

---
+++ install.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env sh
+
+# This script aims to be POSIX compatible (no shell flavor).
+# As a reminder there are no arrays, all variables are global, ...
+
+which_linux()
+{
+	step "Detecting Linux Flavor"
+
+	if has lsb_release; then
+		distro=$(lsb_release -sd)
+	else
+		# try with if [ ! -f /etc/os-release ]; then
+		error "No lsb_release command available"
+		exit 1
+	fi
+
+	if [ "$distro" = '"Void Linux"' ]; then
+		success "$distro detected"
+		distro_file='void_linux.txt'
+		package_manager='xbps-install -y'
+		package_manager_check="$package_manager --dry-run"
+		privilege_escalation='sudo'
+	# elif [ printf "$os_release" | grep -Eq 'debian|ubuntu' ]; then
+	# 	distro_file='debian.txt'
+	# 	package_manager='apt-get install -y'
+	# 	package_manager_check="$package_manager --dry-run"
+	# 	privilege_escalation='sudo'
+	else
+		error "Unhandled flavor for the moment..."
+		exit 1
+	fi
+
+	# printf "$os_release" | grep -q 'arch' \
+	# 	&& package_manager="pacman -S"
+
+	# printf "$os_release" | grep -q 'alpine' \
+	# 	&& package_manager="pkg add "
+
+	# printf "$os_release" | grep -q 'gentoo' \
+	# 	&& package_manager="emerge "
+
+	printf "Package manager command should be '%s'\n" "$package_manager"
+	if ! ask "Is that correct ?"; then
+		exit 1
+	fi
+}
+
+install_profile()
+{
+	if [ ! "$1" ]
+	then
+		error "install_profile function must take a profile as argument"
+		exit 1
+	fi
+
+	### Packages - check
+	# Concatenate all lines representing packages from the current
+	# text file and verify whether the package manager accepts
+	# all of them.
+	packages=$(awk '/^[a-zA-Z0-9]/ {printf "%s ", $0} END{print ""}' \
+		"$1/$distro_file")
+	if ! r=$(eval "$package_manager_check" "$packages" 2>&1 >/dev/null)
+	then
+		error "The package check failed:\n$r"
+		exit 1
+	fi
+	### Packages - install
+	install_command="$privilege_escalation $package_manager $packages"
+	printf "Running: $install_command\n"
+	if ! ask "Proceed ?";
+	then
+		exit 1
+	elif ! eval "$install_command";
+	then
+		error "Something failed during the packages installation."
+		exit 1
+	fi
+
+	### Install config using directives from the profile script
+	# if [ ! "./$1/unpack.sh" ];
+	# then
+	# fi
+
+}
+
+
+
+# Is the script not called from the root of the repository ?
+if [ "$(pwd | awk -F '/' '{print $NF}')" != "os-bootstrap" ];
+then
+	print "Please, run this script while being in the repo directory"
+	exit 1
+# Otherwise load some helper functions
+elif ! . ./utils.sh; then
+	print "utils.sh file not found."
+	exit 1
+fi
+
+
+
+operating_system="$(uname -s)"
+if [ "$operating_system" = "Linux" ]
+then
+
+	which_linux
+
+	install_profile
+
+	success "The script has finished the install. Enjoy."
+
+return 0
+
+# vim: fdm=marker fmr={,}
--- utils.sh
+++ utils.sh
@@ -1,44 +1,72 @@
-# No shebang, this script is not meant to be executed
+# No shebang: This script is just sourced
+#
+# Prevent git from asking to supply information in an interactive way.
+# This makes the check regarding a remote repository possible.
+export GIT_ASKPASS="echo"
+export SSH_ASKPASS="echo"
 
+### Logging
 error()
 {
-    printf "\033[0;1;31m$*\033[0m\n"
-    error=1
+	printf "\033[0;1;31m$*\033[0m\n"
 }
 
 success()
 {
-    printf "\033[0;1;32m$*\033[0m\n"
+	printf "\033[0;1;32m$*\033[0m\n"
 }
 
 warning()
 {
-    printf "\033[0;1;33m$*\033[0m\n"
+	printf "\033[0;1;33m$*\033[0m\n"
 }
 
 info()
 {
-    printf "\033[0;1;34m$*\033[0m\n"
+	printf "\033[0;1;34m$*\033[0m\n"
 }
 
 step()
 {
-    printf "\033[0;1;35m$*\033[0m\n"
+	printf "\033[0;1;35m$*\033[0m\n"
 }
 
-call()
+
+
+check_git_repository()
 {
-    info "$*"
-
-    "$@"
-    ret=$?
-    if [ $ret -ne 0 ]
-    then
-        error "$*\nreturned: $ret"
-		error "Being in: $(pwd)"
-        exit $ret
-    fi
+	### Repository / TODO archive, tar.gz ? ...
+	# --exit-code allows the command to fail if the branch is incorrect
+	if ! git ls-remote --exit-code "$1" "$2" > /dev/null 2>&1;
+	then
+		error "The repository or branch is incorrect"
+		return 1
+	fi
+	return 0
 }
+
+create_file_tree()
+{
+	# test before ?... + Need to be optional
+	# Create the folders (if any) from the profile template description
+	if ! find "$home_template/$1" -type d | xargs -I % mkdir %; then
+		error "Could not create the folders from the $1 profile"
+	fi
+}
+
+# call()
+# {
+# 	info "$*"
+
+# 	"$@"
+# 	ret=$?
+# 	if [ $ret -ne 0 ]
+# 	then
+# 		error "$*\nreturned: $ret"
+# 		error "Being in: $(pwd)"
+# 		exit $ret
+# 	fi
+# }
 
 # The following is taken from: https://github.com/dylanaraps/pfetch
 # This is just a simple wrapper around 'command -v' to avoid
@@ -46,8 +74,8 @@ call()
 # against aliases and functions.
 has()
 {
-    _cmd=$(command -v "$1") 2>/dev/null || return 1
-    [ -x "$_cmd" ] || return 1
+	_cmd=$(command -v "$1") 2>/dev/null || return 1
+	[ -x "$_cmd" ] || return 1
 }
 
 ask()
@@ -60,3 +88,5 @@ ask()
 		return 1
 	fi
 }
+
+
